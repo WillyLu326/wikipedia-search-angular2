400f33336cff327ab32b92b48f86cd6a3dbdd818
WillyLu326 <luzh0326@gmail.com>
2017-02-12 15:48:32 -0500

    Add wikepedia search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WikipediaSearchComponent } from './wikipedia-search/wikipedia-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WikipediaSearchComponent
   ],
   imports: [
     BrowserModule,
